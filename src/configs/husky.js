1d342f531378e39ae95728f610c518e0461e3443
Tomer Aberbach <tomeraberbach@gmail.com>
2021-07-16 00:03:56 -0400

    fix: use path to bin

--- src/configs/husky.js
+++ src/configs/husky.js
@@ -1,10 +1,14 @@
+import { join } from 'path'
+import rootPath from '../root.js'
 import { getConfigPath } from './index.js'
 
+const getBinPath = name => join(rootPath, `node_modules/.bin`, name)
+
 const config = {
-  'commit-msg': `pnpx --no-install commitlint --config ${getConfigPath(
+  'commit-msg': `${getBinPath(`commitlint`)} --config ${getConfigPath(
     `commitlint.json`,
   )} --edit "$1"`,
-  'pre-commit': `pnpx --no-install lint-staged --config ${getConfigPath(
+  'pre-commit': `${getBinPath(`lint-staged`)} --config ${getConfigPath(
     `lint-staged.json`,
   )}`,
 }
