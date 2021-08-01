9c6577095a961cb7f67cf195c08e53b418398e2c
Tomer Aberbach <tomeraberbach@gmail.com>
2021-08-01 16:42:32 -0400

    fix: resolve node deps for deps option

--- src/commands/build.js
+++ src/commands/build.js
@@ -2,6 +2,7 @@ import { join } from 'path'
 import { rollup } from 'rollup'
 import { terser } from 'rollup-plugin-terser'
 import externals from 'rollup-plugin-node-externals'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import maxmin from 'maxmin'
 
 export const command = `build <entry>`
@@ -33,6 +34,7 @@ export const handler = async ({
   const bundle = await rollup({
     input: entry,
     plugins: [
+      deps && nodeResolve(),
       {
         name: `compute-size`,
         transform(code) {
