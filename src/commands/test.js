fad1737c34d006f62f345b0234c1709748e7975e
Tomer Aberbach <tomeraberbach@gmail.com>
2021-09-11 21:37:51 -0400

    feat: color and snapshot update support

--- src/commands/test.js
+++ src/commands/test.js
@@ -7,15 +7,30 @@ export const command = `test`
 export const description = `Runs tests`
 
 export const builder = yargs =>
-  yargs.option(`coverage`, {
-    alias: `c`,
-    type: `boolean`,
-    default: false,
-    description: `run with coverage`,
-  })
+  yargs
+    .option(`coverage`, {
+      alias: `c`,
+      type: `boolean`,
+      default: false,
+      description: `run with coverage`,
+    })
+    .option(`update-snapshots`, {
+      alias: `u`,
+      type: `boolean`,
+      default: false,
+      description: `update snapshots`,
+    })
 
-export const handler = async ({ coverage, projectDirectoryPath }) => {
-  await exec(`${coverage ? `c8 ava` : `ava`} -T 1m`)
+export const handler = async ({
+  coverage,
+  'update-snapshots': updateSnaphots,
+  projectDirectoryPath,
+}) => {
+  await exec(
+    `${coverage ? `c8 ava` : `ava`} -T 1m --color ${
+      updateSnaphots ? `--update-snapshots` : ``
+    }`,
+  )
 
   if (await pathExists(join(projectDirectoryPath, `test-d`))) {
     await exec(`tsd`)
