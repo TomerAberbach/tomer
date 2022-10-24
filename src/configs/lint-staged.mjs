d4963a92bdf4bb18e608e0bc42035040b3234c00
Tomer Aberbach <tomeraberbach@gmail.com>
2022-10-24 19:43:17 -0400

    feat: run typecheck and test regardless of file changed

--- src/configs/lint-staged.mjs
+++ src/configs/lint-staged.mjs
@@ -13,15 +13,12 @@ const [hasTypes, hasTest, useAddlicense] = await Promise.all([
   getUseAddlicense(),
 ])
 
-const srcCommands = [
-  hasTypes && `tomer typecheck --`,
-  hasTest && `tomer test --findRelatedTests --no-watch --passWithNoTests`,
-].filter(Boolean)
-
 export default {
   [`*.{${SRC_EXTENSIONS.join(`,`)},md}`]: [`tomer lint --`],
-  ...(srcCommands.length > 0
-    ? { [`*.{${SRC_EXTENSIONS.join(`,`)}}`]: srcCommands }
-    : {}),
-  '*': [`tomer format --`, useAddlicense && `addlicense`].filter(Boolean),
+  '*': [
+    `tomer format --`,
+    useAddlicense && `addlicense`,
+    hasTypes && `tomer typecheck --`,
+    hasTest && `tomer test --findRelatedTests --no-watch --passWithNoTests`,
+  ].filter(Boolean),
 }
