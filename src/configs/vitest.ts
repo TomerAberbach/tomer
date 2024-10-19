17ea95d137f825c10ad5d0ecc0e95fc1189aa41a
Tomer Aberbach <tomer@aberba.ch>
2024-10-19 16:54:54 -0400

    feat: jest-extended and test setup files (#24)

--- src/configs/vitest.ts
+++ src/configs/vitest.ts
@@ -1,34 +1,52 @@
 import { join } from 'node:path'
 import { defineConfig } from 'vitest/config'
 import etz from 'etz'
+import { asConcur, findConcur, map, mapConcur, orConcur, pipe } from 'lfi'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { getProjectDirectory } from '../helpers/local.js'
+import { getProjectDirectory, hasLocalFile } from '../helpers/local.js'
 import {
   getBrowserslistConfig,
   getConfigPath,
   getTomerConfig,
 } from '../helpers/config.js'
+import type { TomerConfig } from '../helpers/config.js'
 import { stringify } from '../helpers/json.js'
 import { SRC_EXTENSIONS } from '../helpers/matches.js'
 
-const [projectDirectory, { src, test, bench }, browserslistConfig] =
+const getSetupFiles = ({ test }: TomerConfig): Promise<string[]> =>
+  pipe(
+    SRC_EXTENSIONS,
+    map(extension => join(test, `setup.${extension}`)),
+    asConcur,
+    findConcur(hasLocalFile),
+    mapConcur(path => [path]),
+    orConcur((): string[] => []),
+  )
+
+const [projectDirectory, { src, test, bench, setupFiles }, browserslistConfig] =
   await Promise.all([
     getProjectDirectory(),
-    getTomerConfig(),
+    getTomerConfig().then(async config => ({
+      ...config,
+      setupFiles: await getSetupFiles(config),
+    })),
     getBrowserslistConfig(),
   ])
+
+etz.debug(`Test setup files: ${stringify(setupFiles)}`)
 
 const defaultEnvironment = browserslistConfig ? `jsdom` : `node`
 etz.debug(`Default test environemnt: ${stringify(defaultEnvironment)}`)
 
 const srcExtensionsPattern = `{${SRC_EXTENSIONS.join(`,`)}}`
-const exclude = [`**/{node_modules,fixtures,helpers}/**/*`]
+const exclude = [`**/{node_modules,fixtures,helpers}/**/*`, ...setupFiles]
+etz.debug(`Excluded test files: ${stringify(exclude)}`)
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     root: projectDirectory,
-    setupFiles: [getConfigPath(`jest-extended.js`)],
+    setupFiles: [getConfigPath(`jest-extended.js`), ...setupFiles],
     environmentMatchGlobs: [
       [join(test, `dom/**/*`), `jsdom`],
       [join(test, `node/**/*`), `node`],
