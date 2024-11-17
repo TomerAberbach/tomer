9f3156c3b315d2e82037d9cd31178ea5d01d7789
Tomer Aberbach <tomer@aberba.ch>
2024-11-17 16:20:22 -0500

    chore: delete lint-staged and git hooks stuff

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,11 +2,9 @@ import * as bench from './bench.js'
 import * as build from './build.js'
 import * as clean from './clean.js'
 import type { CommandModule } from './command-module.js'
-import * as commitMsg from './commit-msg.js'
 import * as format from './format.js'
 import * as install from './install.js'
 import * as lint from './lint.js'
-import * as preCommit from './pre-commit.js'
 import * as test from './test.js'
 import * as typecheck from './typecheck.js'
 
@@ -14,11 +12,9 @@ const commands: CommandModule[] = [
   bench,
   build,
   clean,
-  commitMsg,
   format,
   install,
   lint,
-  preCommit,
   test,
   typecheck,
 ]
